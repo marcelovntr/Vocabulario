91f8cd61d93579ca0d96a6b3f8918e9bdf15efaa
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2025-05-12 00:15:42 -0300

    adição da página de atualizar e as funções de atualizar e deletar com o 'mínimo viável'

--- vocabulario/vocabulario_app/urls.py
+++ vocabulario/vocabulario_app/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create_vocabulary, name='create_vocabulary'),
-    path('list/', views.list_vocabulary, name='list_vocabulary')
+    path('list/', views.list_vocabulary, name='list_vocabulary'),
+    path('update/<int:id>', views.update_vocabulary, name='update_vocabulary'),
+    path('delete/<int:id>', views.delete_vocabulary, name='delete_vocabulary')
 ]
--- vocabulario/vocabulario_app/views.py
+++ vocabulario/vocabulario_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Word
 from django.contrib import messages
 
@@ -26,3 +26,35 @@ def create_vocabulary(request):
 def list_vocabulary(request):
     words = Word.objects.all()
     return render(request, 'vocabulario_app/pages/list_vocabulary.html', context={'words': words})
+
+def  delete_vocabulary(request,id):
+    #ou: get_object_or_404
+    word_found = Word.objects.get(id=id)
+    try:
+        word_found.delete()
+        messages.success(request, f"Palavra: {word_found.vocabulo} deletada com sucesso!")
+    # except Word.DoesNotExist:
+    #     messages.error(request, f"Palavra: {word_found.vocabulo} não encontrada!")
+    # except Word.MultipleObjectsReturned:
+    #     messages.error(request, f"Palavra: {word_found.vocabulo} não encontrada!")
+    except Exception:
+        messages.error(request, f"Erro ao deletar palavra: {word_found.vocabulo}")
+        
+    return redirect('list_vocabulary')
+
+def update_vocabulary(request, id):
+    if request.method == 'GET':
+        word_found = Word.objects.get(id=id)
+        return render(request, 'vocabulario_app/pages/update_vocabulary.html', context={'found': word_found})
+    
+    if request.method == 'POST':
+        vocabulo = request.POST.get('vocabulo')
+        significado = request.POST.get('significado')
+        exemplo = request.POST.get('exemplo')
+        frequencia = request.POST.get('frequencia')
+
+    Word.objects.filter(id=id).update(
+            vocabulo=vocabulo,significado=significado,exemplo=exemplo,frequencia=frequencia
+            )
+    messages.success(request, f"Dados da palavra: {vocabulo}")
+    return redirect('list_vocabulary')
